53391edbed91f3f37ec3be9734752900b33623d9
LimHM <gusals3407@gmail.com>
2017-02-18 11:48:24 +0900

    첫 단위 테스트: url 맵핑, 임시 뷰 생성

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,6 +1,10 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from lists.views import home_page
 
 
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+    def test_root_url_reslove_to_home_page_view(self):
+        # resolve는 장고 내부 함수로, URL을 해석해 일치하는 뷰 함수를 찾는다.
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
