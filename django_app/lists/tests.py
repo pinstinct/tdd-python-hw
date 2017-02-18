b1c420091f0110a913713f8768ccb17a1006281f
LimHM <gusals3407@gmail.com>
2017-02-18 17:09:12 +0900

    POST 요청을 처리하도록 뷰와 템플릿 변경 기능테스트, 단위 테스트도 수정

--- django_app/lists/tests.py
+++ django_app/lists/tests.py
@@ -26,3 +26,20 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('lists/home.html')
         # .decode를 이용해 response.content 바이트 데이터를 파이썬 유니코드 문자열로 변환
         self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        # 테스트 설정을 위한 코드
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+
+        # 실제 함수 호출
+        response = home_page(request)
+
+        # Assertion 코드
+        self.assertIn('신규 작업 아이템', response.content.decode())
+        expected_html = render_to_string(
+            'lists/home.html',
+            {'new_item_text': '신규 작업 아이템'}
+        )
+        self.assertEqual(response.content.decode(), expected_html)
--- django_app/lists/views.py
+++ django_app/lists/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'lists/home.html')
+    # dict.get(key, default=None)
+    # key : dict에서 key를 검색
+    # default : key가 없을 경우, default를 반환
+    return render(request, 'lists/home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+    })
