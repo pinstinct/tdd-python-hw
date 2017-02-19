d58bb17bfe6a06dfaf22e72d775aa9b84c2401f8
LimHM <gusals3407@gmail.com>
2017-02-19 09:47:36 +0900

    form에서 url을 사용하도록 수정, FT 성공

--- django_app/lists/tests.py
+++ django_app/lists/tests.py
@@ -58,6 +58,12 @@ class ListAndItemModelsTest(TestCase):
 
 
 class ListViewTest(TestCase):
+    def test_passes_correct_list_to_tempate(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get('/lists/{}/'.format(correct_list.id,))
+        self.assertEqual(response.context['list'], correct_list)
+
     def test_uses_list_template(self):
         list_ = List.objects.create()
         response = self.client.get('/lists/{}/'.format(list_.id, ))
