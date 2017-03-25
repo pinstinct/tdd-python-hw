1f5de2a2387baa31d300259f4e404edbb46a0113
LimHM <gusals3407@gmail.com>
2017-03-26 00:27:05 +0900

    레이아웃 개선을 위한 부트스트랩 사용

--- django_app/superlists/settings.py
+++ django_app/superlists/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATE_DIRS = (TEMPLATE_DIR,)
@@ -81,3 +82,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    STATIC_DIR,
+)
