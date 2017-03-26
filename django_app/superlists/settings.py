f65de10a0c42a4b98186b51341c5a88c2b4b532c
LimHM <gusals3407@gmail.com>
2017-03-26 22:27:23 +0900

    sqlite 데이터베이스를 메인 소스 밖으로 이동

--- django_app/superlists/settings.py
+++ django_app/superlists/settings.py
@@ -8,7 +8,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -16,22 +15,18 @@ ROOT_DIR = os.path.dirname(BASE_DIR)
 
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATE_DIRS = (TEMPLATE_DIR,)
+
 STATIC_ROOT = os.path.join(ROOT_DIR, 'static_root')
+STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    STATIC_DIR,
+)
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '6)hgb_!%t7u&(2=5y6an4l$eb$-x8q$r&46hkb#bz1kgyoxcjy'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 TEMPLATE_DEBUG = True
-
 ALLOWED_HOSTS = []
-
-# Application definition
 
 INSTALLED_APPS = (
     # 'django.contrib.admin',
@@ -60,32 +55,17 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(ROOT_DIR, 'database/db.sqlite3'),
     }
 }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
-STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
-    STATIC_DIR,
-)
